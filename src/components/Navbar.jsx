0dad45b8a4e66b5a35d997ed3afe21a56a612609
shyamg090 <samshyam9630@gmail.com>
2024-09-30 21:27:08 +0530

    Darkmode button Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { getConfigData } from "../data/configReader";
 
@@ -9,10 +10,12 @@ import { MdOutlineNightlightRound } from "react-icons/md";
 export default function Navbar() {
   const configData = getConfigData();
 
-  const [theme, settheme] = useState('dark')
+  const themeIcon = [<MdOutlineLightMode className="w-[3vw] h-[3vh]" />  , <MdOutlineNightlightRound className="w-[3vw] h-[3vh]" />]
 
-  const mode = (e)=>{
-    return
+  const [theme, settheme] = useState(false)
+
+  const changeTheme = (e)=>{
+    return settheme(!theme)
   }
 
   return (
@@ -117,7 +120,12 @@ export default function Navbar() {
                 </svg>
               </NavLink>
             </div>
-            <div className="justify-center">
+
+
+            <div className="justify-center flex">
+
+            <div className="items-center flex justify-center" onClick={changeTheme}> {theme ? themeIcon[1] : themeIcon[0]} </div>
+
               <a href={configData.hireMeLink}><button
                 type="button"
                 className="gap-x-1 before:ease relative overflow-hidden border border-[#000000] bg-[#050708] text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:before:-translate-x-40 font-medium rounded-lg text-sm px-2 py-2 text-center inline-flex items-center mx-7"
@@ -136,7 +144,6 @@ export default function Navbar() {
                     d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
                   />
                 </svg>
-                <span className="hidden md:block"></span>
 
                 <span className="hidden md:block">Hire Me</span>
               </button></a>
